9d45deeb9d2934fe1bad861a9418ba97b0c21afb
Morideaux <Morideaux7@gmail.com>
2024-06-03 18:49:52 -0700

    tutoring session adjustments restuctered for string interpolation. additionally corrected links to css and jss flies

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,59 +1,75 @@
-function CreateBlogPost(username, blogTitle, blogContent) {
-        this.username = username;
-        this.Blog-title = blogTitle;
-        this.Blog-content = blogContent;
-    }
+// const blogPost = require('./blog.js')
+// console.log(blogPost)
 
-    //sets up blogPost capabilities
-    BlogPost.prototype = {
-        submition: function () {
-            const data = new FormData(event.target);
-            const formEntry = Object.formEntries(data.entries());
+// function CreateBlogPost(username, blogTitle, blogContent) {
+//         this.username = username;
+//         this.Blog-title = blogTitle;
+//         this.Blog-content = blogContent;
+//     }
 
-            const postedUser = data.get('username');
-            const postedTitle = data.get('blog-title');
-            const postedContent = data.get('blog-content');
+//     //sets up blogPost capabilities
+//     BlogPost.prototype = {
+//         submition: function () {
+//             const data = new FormData(event.target);
+//             const formEntry = Object.formEntries(data.entries());
 
-            //should this be a seperate function
-            var previousPosts = {value1: `${postedUser}`, value2: `${postedTitle}`, value3: `${postedContent}`}
-            localStorage.setItem( previousPostsIndex, JSON.stringify(previousPosts))
+//             const postedUser = data.get('username');
+//             const postedTitle = data.get('blog-title');
+//             const postedContent = data.get('blog-content');
 
-            //does return need to be followed by somthing specific?
-            return 
-        },
+//             //should this be a seperate function
+//             var previousPosts = {value1: `${postedUser}`, value2: `${postedTitle}`, value3: `${postedContent}`}
+//             localStorage.setItem( previousPostsIndex, JSON.stringify(previousPosts))
 
-        //intended to read localstorage JSON and save blog post array for displaying
-        posted: function () {
-            const blogPost = {}
-                for(var i=0; i<localStorage.length; i++) {
-                var previousPosts = localStorage.previousPosts( i );
-                var blogPost = json.parse( localStorage.getItem(previousPosts));
-            }
-        },
+//             //does return need to be followed by somthing specific?
+//             return 
+//         },
 
-        //needs to get post count seperate each post as obj. then give each a card in the html.
-        renderBlogPost: function () {
-            BlogPost.innerHTML = '';
+//         //intended to read localstorage JSON and save blog post array for displaying
+//         posted: function () {
+//             const blogPost = {}
+//                 for(var i=0; i<localStorage.length; i++) {
+//                 var previousPosts = localStorage.previousPosts( i );
+//                 var blogPost = json.parse( localStorage.getItem(previousPosts));
+//             }
+//         },
 
-            for (let i = 0; i < blogPost.length; i++) {
-                const blogPost = blogPost[i];
+//         //needs to get post count seperate each post as obj. then give each a card in the html.
+//         renderBlogPost: function () {
+//             BlogPost.innerHTML = '';
 
-                const li = document.createElement('li');
-                li.textContent = blogPost
-                li.setAttribute('uncertain////////', i);
+//             for (let i = 0; i < blogPost.length; i++) {
+//                 const blogPost = blogPost[i];
 
-            }
-        },
+//                 const li = document.createElement('li');
+//                 li.textContent = blogPost
+//                 li.setAttribute('uncertain////////', i);
 
-        // not needed seems nice
-        terminateBlogPost: function () {
-            const button = document.getElementById('ive not set this up');
-            button.addEventListener('click', function() {
-                //remove from local storage json array and page
-            }); 
-        },
+//             }
+//         },
+
+//         // not needed seems nice
+//         terminateBlogPost: function () {
+//             const button = document.getElementById('ive not set this up');
+//             button.addEventListener('click', function() {
+//                 //remove from local storage json array and page
+//             }); 
+//         },
 
 
-    }
+//     }
+const blogSection = document.querySelector("#blog-section")
+const blogPosts = JSON.parse(localStorage.getItem("blogArray")) || []
 
+let blogCard = ""
+blogPosts.forEach(post => {
+blogCard += `
+    <div class="blog-card">
+    <h2>${post.title}</h2>
+    <p>${post.username}</p>
+    <p>${post.content}</p>
+    </div>
+`;
+blogSection.innerHTML = blogCard
+});
     
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,19 @@
-class blogPost {
-    constructor(username, blogTitle, blogContent) {
-        this.label.username = username;
-        this.label.Blog-title = blogTitle;
-        this.label.Blog-content = blogConten;
+const submitForm = (event) => {
+    event.preventDefault()
+    const blogArray = JSON.parse(localStorage.getItem("blogArray")) || []
+    const username = document.querySelector("#username").value
+    const title = document.querySelector("#blog-title").value
+    const blogContent = document.querySelector("#blog-content").value
+
+    const newBlog = {
+        username: username,
+        title: title,
+        content: blogContent,
     }
+    blogArray.push(newBlog)
+    localStorage.setItem("blogArray", JSON.stringify(blogArray))
+    document.location.href = "blog.html"
 }
 
-const 
+const blogButton = document.querySelector("#post-btn")
+blogButton.addEventListener("click", submitForm) 
